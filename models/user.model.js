17fd7391cea76e9bdef63239c16c85a25779abab
Shahrear Ahamed <its.shahrear@gmail.com>
2022-11-23 18:21:03 +0600

    make user schema

---
+++ models/user.model.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const userSchema = Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 150,
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: 8,
+      maxlength: 100,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.generateToken = function () {
+  const jwtToken = jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+    },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: "7d" }
+  );
+};
+
+const users = model("User", userSchema);
+
+module.exports = users;
